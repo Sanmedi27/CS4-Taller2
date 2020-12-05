c598d7dcb34d5a2e48e6ebf869551390cbb3fd04
SantiagoMedinaNRA <santiago.medina@numrot.com>
2020-12-05 16:14:18 -0500

    Se crea Header.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import Header from './components/Header'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+    <Container>
+        <Row>
+          <Col xs={6} md={4}></Col>
+          <Col xs={6} md={4}>
+            
+          </Col>
+          <Col xs={6} md={4}></Col>
+        </Row>
+        <Row>
+          <Col>
+            
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
