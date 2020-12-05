f7f0fdc210a8a802311476f8bdde7d239a07a0de
SantiagoMedinaNRA <santiago.medina@numrot.com>
2020-12-05 17:52:32 -0500

    Se arregla identacion.

--- src/components/AnimeItems.jsx
+++ src/components/AnimeItems.jsx
@@ -1,16 +1,23 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Container, Row } from "react-bootstrap";
+import ShorMore from "./ShorMore";
 
 const AnimeItems = ({ anime }) => {
   return (
     <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={anime.image_url} />
+      <Card.Img variant="top" src={anime.image_url} height="300" />
       <Card.Body>
         <Card.Title>{anime.title}</Card.Title>
-        <Card.Text>{anime.synopsis}</Card.Text>
-        <a href={anime.url} variant="primary" target="blank">
-          Continue Reading
-        </a>
+        <Container>
+          <Row className="justify-content-md-center">
+            <ShorMore sinopsis={anime.synopsis} />
+          </Row>
+          <Row className="justify-content-md-center">
+            <a href={anime.url} variant="dark" target="blank">
+              MyAnimeList
+            </a>
+          </Row>
+        </Container>
       </Card.Body>
     </Card>
   );
